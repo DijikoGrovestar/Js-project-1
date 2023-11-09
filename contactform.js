deaf0c6423e9414227c5eff1cfa5589c96f2aa89
DijikoGrovestar <saritha.kolanupaka@healthonus.io>
2023-11-09 10:51:12 +0530

    applied validation to email field

--- contactform.js
+++ contactform.js
@@ -66,6 +66,17 @@ function validateForm() {
   }
 }
 
+//validate Email Address
+function validateEmail(emailField){
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
+  if (reg.test(emailField.value) == false) 
+  {
+      alert('Invalid Email Address');
+      return false;
+  }
 
+  return true;
+
+}
 
